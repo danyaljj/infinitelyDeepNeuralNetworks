2bd0cd49a1d433e9ea432b69614f63081c72ce36
Daniel Khashabi <khashab2@illinois.edu>
2018-06-12 17:09:46 -0400

    Create infinitely_deep_neural_networks.py

---
+++ infinitely_deep_neural_networks.py
@@ -0,0 +1,8 @@
+## The whole code is in Python woooo 
+
+import unittest
+def print_the_final_results():
+  pass ## finisih this code later; after the paper publication, maybe. 
+  
+if __name__ == '__main__':
+    print_the_final_results()
